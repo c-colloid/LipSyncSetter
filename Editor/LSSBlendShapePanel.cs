3737b37f1d736f024e9111bfec5939618c8a1a44
colloid <jack0904h3@yahoo.co.jp>
2025-01-15 16:57:43 +0900

    Delete unused codes

--- Editor/LSSBlendShapePanel.cs
+++ Editor/LSSBlendShapePanel.cs
@@ -51,14 +51,11 @@ namespace LipSyncSetter
 				targetfield.RegisterValueChangedCallback(evt => 
 					targetfield.style.backgroundColor = targetfield.index > 0 ? default : warningcolor);
 				ve.Q<PopupField<string>>().Q<VisualElement>(null,"unity-base-popup-field__input").pickingMode = PickingMode.Position;
-				//ve.Q<PopupField<string>>().Q<VisualElement>(null,"unity-base-popup-field__input")
-				//	.AddManipulator(new Editor.Manipulator.ClickFieldManipulator());
 			};
 			
 			//FaceMeshオブジェクトフィールドを変更した時
 			root.Q<ObjectField>("FaceMesh").RegisterValueChangedCallback(evt =>
 			{
-				//if (!AvatarDescriptor) return;
 				var mesh = evt.newValue as SkinnedMeshRenderer;
 				var textlist = root.Query<VisualElement>(null,null,"myfield").ToList();
 				LipSyncBlendShape.Clear();
@@ -74,28 +71,7 @@ namespace LipSyncSetter
 				for (int j = 0; j < target.blendShapeCount ; j++) {
 					LipSyncBlendShape.Add(target.GetBlendShapeName(j));
 				}
-			
-				//Generate ListViewItem
-				//foreach (var item in _avatrDescriptor.VisemeBlendShapes)
-				//{
-				//	//Debug.Log(item);
-				//	if (i < textlist.Count)
-				//	{
-				//		var oldfield = textlist[i];
-				//		var blendshapeindex = 0;
-				//		if (_avatrDescriptor.VisemeSkinnedMesh == mesh)
-				//			blendshapeindex = LipSyncBlendShape.IndexOf(_avatrDescriptor.VisemeBlendShapes[i]);
-				//		var newfield = new PopupField<string>(oldfield.Query<Label>().First().text,LipSyncBlendShape,blendshapeindex);
-				//		newfield.AddToClassList("myfield");
-				//		root.Q<ListView>("LipSyncList").Remove(oldfield);
-				//		root.Q<ListView>("LipSyncList").Add(newfield);
-				//	}
-				//	i++;
-				//}
-				//root.Q<ListView>("LipSyncList").Remove(textlist.Last());
-				//var field = new PopupField<string>("Disable",LipSyncBlendShape,"disable");
-				//field.AddToClassList("myfield");
-				//root.Q<ListView>("LipSyncList").Add(field);
+
 				root.Q<ListView>("LipSyncList").Rebuild();
 			});
 		}
